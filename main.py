96e77c391752a441f673ed6dcb80e5e1fa9471e9
mit raval <hareshraval8140@gmail.com>
2026-06-10 15:28:37 +0530

    create dlt and update function

--- main.py
+++ main.py
@@ -20,12 +20,13 @@ class system:
             elif choice==2:
                 self.view_details()
             elif choice==3:
-                pass
+                self.update()
             elif choice==4:
-                pass
+                self.dlt_details()
             else:
                 break
                 exit()
+
     def add_details(self):
         name1=input("ENTER YOUR NAME:")
         self.details["name"]=name1
@@ -34,14 +35,57 @@ class system:
         subjects=int(input("ENTER YOUR NO OF SUBJECTS:"))
         for i in range(subjects):
             names=input(f"ENTER {i+1} SUBJECT NAME=")
-            marks=int(input(f"ENTER {names} SUBJECT MARK="))
-            self.details["marks"][names]=marks
+            marks01=int(input(f"ENTER {names} SUBJECT MARK="))
+            self.details["marks"][names]=marks01
         print(self.details)
+
+
     def view_details(self):
-        if self.details["name"]:
-            print(self.details)
+        if self.details=="":
+            # print(self.details)
+            print("no data")
         else:
             print("THERE ARE NO DETAILS IN LIST")
+            print(self.details)
+
+
+    def update(self):
+        if self.details=="":
+            print("THERE ARE NO DETAILS IN LIST TO UPDATE")
+        else:
+            updates=input("what you update=")
+            if updates=="name":
+                upadte01=input(f"enter your new {updates}=")
+                self.details["name"]=upadte01
+            if updates=="roll-no":
+                upadte01=input(f"enter your new {updates}=")
+                self.details["roll-no"]=upadte01
+            if updates=="marks":
+                subjects=input("enter subject name to update=")
+                for i in self.details["marks"]:
+                    update_mark=int(input("ENTER UPDATED marks="))
+                    self.details["marks"][subjects]=update_mark
+                    print("marks updated successfully")
+ 
+            
+
+    def dlt_details(self):
+        if self.details=="":
+            print("there are no details to delete")
+        else:
+            dlt=input("what you delete=")
+            if dlt=="name":
+                self.details["name"]=""
+                print("name deleted")
+            if dlt=="roll-no":
+                self.details["roll-no"]=""
+            if dlt=="marks":
+                subjects=input("enter subject name to delete marks=")
+                for i in self.details["marks"]:
+                    self.details["marks"][subjects]=""
+                    print("marks deleted successfully")
+ 
+            
             
 
 
